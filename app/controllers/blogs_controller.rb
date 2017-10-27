626c838ada55f28df9597525c204a1400e2b8404
bradmoaalii <brad.moaalii@gmail.com>
2017-10-27 13:42:32 -0600

    implemented partials for blog font aswesome and pagination

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.limit(2)
+    @blogs = Blog.page(params[:page]).per(5)
     puts @blogs.inspect
     @page_tile = "My Portfolio Blog"
   end
