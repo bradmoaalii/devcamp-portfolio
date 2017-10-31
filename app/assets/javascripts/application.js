5ead14992f40a638feda8c86bfd202c6d8d2f4af
bradmoaalii <brad.moaalii@gmail.com>
2017-10-31 09:47:10 -0600

    messing around with Navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,12 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+
+$(document).ready(function() {
+  $(".scroll").click(function(event) {
+    event.preventDefault();
+    $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 500);
+    $(".navbar-default a").removeClass("selected");
+    $(this).addClass("selected");
+  });
+});
