309b4eadea6754970ac44c8836392e26ba491cb2
bradmoaalii <brad.moaalii@gmail.com>
2017-10-31 14:08:37 -0600

    installed new gems and put in new forms for show page and portfolio edit page

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,6 +6,7 @@ class Portfolio < ApplicationRecord
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+
   def self.angular
     where(subtitle: 'Angular')
   end
