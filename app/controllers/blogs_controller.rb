e4fc49c0fba9905957a5cad15da51dcb3cc93b20
bradmoaalii <brad.moaalii@gmail.com>
2017-11-07 14:53:29 -0700

    organized the blogs view by changing to refresh with most recent

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,11 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5)
-    puts @blogs.inspect
+    if logged_in?(:site_admin)
+    @blogs = Blog.recent.page(params[:page]).per(5)
+    else
+      @blogs=Blog.published.recent.page(params[:page]).per(5)
+    end
     @page_tile = "My Portfolio Blog"
   end
 
